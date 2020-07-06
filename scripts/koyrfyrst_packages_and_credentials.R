8dd44f96659ec826a8c65765046fd2ed994c4f3d
BgAndreasen <bir_and@hotmail.com>
2020-07-06 03:25:15 +0100

    indexfunktiónir og indexkort

--- scripts/koyrfyrst_packages_and_credentials.R
+++ scripts/koyrfyrst_packages_and_credentials.R
@@ -3,14 +3,16 @@
 # installera neyðugu r-pakkarnar
 
 #packages required
-uttrekk_pack <- c("tidyverse", "lubridate", "shiny", "odbc", "knitr", "kableExtra",
+uttrekk_pack <- c("tidyverse", "lubridate", "shiny", "odbc", "DBI", "knitr", "kableExtra",
                   "ggforce", "ggpubr", "ggrepel", "usethis", "sf")
 mangla <- uttrekk_pack[!uttrekk_pack %in% installed.packages()[,"Package"]]
 install.packages(mangla)
 
 # set up localan .Renviron fíl við databasu upplýsingum og brúkara upplýsingum
 usethis::edit_r_environ("project")
-Sys.setenv(SERVER = rstudioapi::showPrompt(title = "Server", message = "Server navn:", default = ""))
-Sys.setenv(DATABASE = rstudioapi::showPrompt(title = "Database", message = "Database navn:", default = ""))
-Sys.setenv(USER = rstudioapi::showPrompt("Database username", "username"))
-Sys.setenv(PASSWORD = rstudioapi::askForPassword("Database password"))
+
+# kopiera hetta inn í .Renviron fílin, við tínum upplýsingum
+SERVER=navn_ella_ip_adressa_á_servaranum
+DATABASE=navn_á_dátugrunni
+USER=brúkari
+PASSWORD=loyniorð
--- uttrekk.R
+++ uttrekk.R
@@ -2,7 +2,7 @@
 
 
 #her skrivar tú hvat fyri aliøki tú skal hyggja at
-alioki <- "A43"
+alioki <- "A11"
 
 #her skrivar tú hvussu nógvar útsetur tú skal hyggja at
 utsetur <- 3
